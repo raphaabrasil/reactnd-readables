87ef8c39c19b1ec7137f6ba3f2b3ea800afbb528
Raphael Brasil <raphael.brasil@resultadosdigitais.com.br>
2018-11-13 20:22:27 -0200

    Creates a component to  list the categories

---
+++ src/components/category/ListCategories.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { fetchCategories } from './actions'
+
+class ListCategories extends Component {
+  componentDidMount() {
+    !this.props.categories.items.length && this.props.fetchCategories()
+  }
+
+  render() {
+    const { categories } = this.props
+    let content = ''
+    if ( categories.items.length ) {
+      content = (
+         <ul>
+          { categories.items.map( category => (
+            <li key={category.path}>{ category.name }</li>
+          ))}
+        </ul>
+      )
+    }
+    return (
+      <div className="App">
+        { content }
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ categories }) => {
+  return {
+    categories,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCategories: () => dispatch( fetchCategories() ),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListCategories)
