bfa44ac7ebafb5cb545b809db325789e228db1e8
Raphael Brasil <raphael.brasil@resultadosdigitais.com.br>
2018-11-13 20:17:58 -0200

    Uses helper

--- src/api/category.js
+++ src/api/category.js
@@ -1,12 +1,4 @@
-const apiUrl = 'http://localhost:3001'
-let token = localStorage.token
-if (!token)
-  token = localStorage.token = Math.random().toString(36).substr(-8)
-
-const headers = {
-  'Accept': 'application/json',
-  'Authorization': token
-}
+import { apiUrl, headers } from './helper'
 
 export const getCategories = () => (
   fetch ( `${apiUrl}/categories`, { headers } )
