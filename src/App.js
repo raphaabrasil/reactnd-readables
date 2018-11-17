5a00e7475d91aa5db11848acf66550fd486dc63d
Raphael Brasil <raphael.brasil@resultadosdigitais.com.br>
2018-11-17 16:35:52 -0200

    Creates route do CategoryPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import Home from './views/Home'
+import CategoryPage from './views/CategoryPage'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Route exact path="/" component={ Home } />
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route exact path="/:category" component={ CategoryPage } />
+      </Switch>
     )
   }
 }
