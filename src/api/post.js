afb0717bd30a2340254dbe5bb9a1518b95c96377
Raphael Brasil <raphael.brasil@resultadosdigitais.com.br>
2018-11-17 16:34:34 -0200

    Creates function to get posts by category on api

--- src/api/post.js
+++ src/api/post.js
@@ -1,6 +1,11 @@
 import { apiUrl, headers } from './helper'
 
 export const getPosts = () => (
-  fetch ( `${apiUrl}/posts`, { headers } )
+  fetch ( `${ apiUrl }/posts`, { headers } )
+    .then( res => res.json() )
+)
+
+export const getPostsByCategory = category => (
+  fetch ( `${ apiUrl }/${ category }/posts`, { headers } )
     .then( res => res.json() )
 )
