4550001c1fd6a43069164575d9370eaaba947ffc
Raphael Brasil <raphael.brasil@resultadosdigitais.com.br>
2018-11-17 16:34:56 -0200

    Creates actions for posts by category

--- src/components/category/actions.js
+++ src/components/category/actions.js
@@ -1,6 +1,8 @@
 import * as CategoryAPI from '../../api/category'
+import * as PostAPI from '../../api/post'
 
 export const GET_CATEGORIES = 'GET_CATEGORIES'
+export const GET_CATEGORY_POSTS = 'GET_CATEGORY_POSTS'
 
 
 export const getCategories = categories => (
@@ -13,4 +15,17 @@ export const getCategories = categories => (
 export const fetchCategories = () => dispatch => {
   CategoryAPI.getCategories()
     .then( categories => dispatch( getCategories( categories ) ) )
+}
+
+export const getCategoryPosts = ( category, posts ) => (
+  {
+    type: GET_CATEGORY_POSTS,
+    category,
+    posts,
+  }
+)
+
+export const fetchCategoryPosts = category => dispatch => {
+  PostAPI.getPostsByCategory( category )
+    .then( posts => dispatch( getCategoryPosts( category, posts ) ) )
 }
