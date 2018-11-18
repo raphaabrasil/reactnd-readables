8224852c2ceb2e0b2924728dced968f6bed2d631
Raphael Brasil <raphael.brasil@resultadosdigitais.com.br>
2018-11-18 00:49:04 -0200

    Creates comments component

---
+++ src/components/comments/Comments.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import Moment from 'react-moment'
+import { connect } from 'react-redux'
+import moment from 'moment'
+import sortBy from 'sort-by'
+import { fetchPostComments } from './actions'
+
+class Comments extends Component {
+
+  componentDidMount() {
+    const { postId } = this.props
+    this.props.fetchPostComments( postId )
+    console.log(this.props)
+  }
+
+  postComments = () => {
+    const { postsComments, postId } = this.props
+    if (postsComments && postsComments[postId]) {
+      return postsComments[postId].items
+    }
+  }
+
+  render() {
+    const { postsComments, postId } = this.props
+    const comments = this.postComments()
+    let content = (<p>{postId}</p>)
+    if ( comments ) {
+      content = comments.sort( sortBy( '-voteScore' ) ).map( comment => (
+        <div>
+          <p>{ comment.author }</p>
+          <p>{ comment.body }</p>
+          <p>Votes: { comment.voteScore }</p>
+          <Moment
+            format="DD/MM/YYYY HH:mm">
+            {comment.timestamp}
+          </Moment>
+        </div>
+      ) )
+    }
+    return (
+      <div className="comments">
+        <h1>Comments</h1>
+        { content }
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ postsComments }) => {
+  return {
+    postsComments,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchPostComments: ( postId ) => dispatch( fetchPostComments( postId ) ),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Comments)
