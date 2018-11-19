aee720647330d124722aa414b30656d53efa09da
Raphael Brasil <raphael.brasil@resultadosdigitais.com.br>
2018-11-19 16:08:47 -0200

    Creates route for post creation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from './views/Home'
 import CategoryPage from './views/CategoryPage'
 import PostPage from './views/PostPage'
-import './App.css';
+import CreatePost from './views/CreatePost'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/post/create" component={ CreatePost } />
         <Route exact path="/:category" component={ CategoryPage } />
         <Route exact path="/:category/:postId" component={ PostPage } />
       </Switch>
