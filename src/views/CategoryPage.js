c36e4ccd05bf9d58734fca0cefd2ac7a3039fc61
Raphael Brasil <raphael.brasil@resultadosdigitais.com.br>
2019-02-18 20:28:23 -0300

    Uses new sort component on category page

--- src/views/CategoryPage.js
+++ src/views/CategoryPage.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import sortBy from 'sort-by'
 import Button from '@material-ui/core/Button'
 import AddIcon from '@material-ui/icons/Add'
 import { fetchCategoryPosts } from '../components/category/actions'
 import Post from '../components/post/Post'
+import { dynamicSort } from '../utils/sorter'
+import Sort from '../components/interactions/Sort'
+
 
 class CategoryPage extends Component {
-  state = {
-    sortParam: '-voteScore'
-  }
+	state = {
+		category: '',
+	}
 
   componentDidMount() {
     const { category } = this.props.match.params
@@ -25,21 +27,16 @@ class CategoryPage extends Component {
     return (categoryPosts && categoryPosts.posts.length)
   }
 
-  changeOrder = sortParam => (
-    this.setState({
-      sortParam
-    })
-  )
-
   render() {
     const { categoriesPosts } = this.props
-    const { sortParam, category } = this.state
+    const { category } = this.state
     const categoryPosts = categoriesPosts[category]
+		const sortParam = this.props.interactions.sort
     let content = (<p>No posts found for {category}</p>)
     if ( this.hasPostOnCategory() ) {
       content = (
         <div style={{ display: 'flex', flexWrap: 'wrap', alignContent: 'space-around' }}>
-          { categoryPosts.posts.sort( sortBy( sortParam ) ).map( post => (
+          { categoryPosts.posts.sort( dynamicSort( sortParam ) ).map( post => (
             <Post post={ post } />
           )  )  }
         </div>
@@ -53,26 +50,18 @@ class CategoryPage extends Component {
           <Button component= {Link} to={'/post/create'}variant="fab" color="secondary" aria-label="Add" mini>
             <AddIcon />
           </Button>
+					<Sort />
         </div>
-        <p>
-          Order by:
-          <span onClick={ () => this.changeOrder( '-voteScore' ) }>
-            vote score
-          </span>
-          |
-          <span onClick={ () => this.changeOrder( 'title' ) }>
-            Title
-          </span>
-        </p>
         { content }
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ categoriesPosts }) => {
+const mapStateToProps = ({ categoriesPosts, interactions }) => {
   return {
-    categoriesPosts,
+		categoriesPosts,
+		interactions,
   }
 }
 
