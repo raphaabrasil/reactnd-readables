d3ac8cc487d62a77479e7ac784fc606c2b4193d8
Raphael Brasil <raphael.brasil@resultadosdigitais.com.br>
2019-03-12 17:57:46 -0300

    Makes home an stateless component

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ListCategories from '../components/category/ListCategories'
 import ListPosts from '../components/post/ListPosts'
 import Header from '../components/header/Header'
 
-class Home extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <div className='container'>
-          <ListCategories />
-          <ListPosts />
-        </div>
-      </div>
-    )
-  }
-}
+const Home = () => (
+  <div>
+    <Header />
+    <div className='container'>
+      <ListCategories />
+      <ListPosts />
+    </div>
+  </div>
+)
+
 
 export default Home
